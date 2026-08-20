11b2d44764563412f87bf4afaa234f02e4513c90
JoshDow-16 <m291920@usna.edu>
2026-08-20 10:39:54 -0400

    msg

--- lab/l1/jad.cpp
+++ lab/l1/jad.cpp
@@ -1,8 +1,17 @@
+<<<<<<< HEAD
 #incldue <iostream>
+=======
+#include <iostream>
+>>>>>>> 15315e9 (msg)
 using namespace std;
 
 int main()
 {
+<<<<<<< HEAD
+=======
+  cin>>"First Number: ">>int a;
+
+>>>>>>> 15315e9 (msg)
 
   return 0;
 }
