f8a1dd778d6ccaf9b661d523489e03fe07ac8d2d
JoshDow-16 <m291920@usna.edu>
2026-08-20 11:31:53 -0400

    msg

--- lab/l1/jad.cpp
+++ lab/l1/jad.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int main()
 {
   int a,b;
-  cout<<"First Number: ";cin>>a;o
+  cout<<"First Number: ";cin>>a;
   cout<<"Second Number: ";cin>>b;
   cout<<"The sum of these two integers is: "<<a+b<<endl;
   return 0;
+change
 }
