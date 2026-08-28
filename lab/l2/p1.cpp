121b318728dbe8b1e0f015df1d30f952bef63c3f
JoshDow-16 <m291920@usna.edu>
2026-08-28 13:26:19 -0400

    msg

--- lab/l2/p1.cpp
+++ lab/l2/p1.cpp
@@ -5,11 +5,9 @@ using namespace std;
 int main ()
 {
   // Prompt User and Grab Input
-  int bin=0; string binCopy;
-	cout << "Enter a 4-bit binary number: "; cin >> binCopy;
-  cout << binCopy << endl;
-  //bin = binCopy; 
-  1 >> bin;
+  int bin=0;
+	cout << "Enter a 4-bit binary number: "; cin >> bin;
+
   // Process 4-Bit Binary
   int res = ( bin / 1000 )*8 + ( bin % 1000 / 100 )*4 + ( bin % 100 / 10 )*2 + ( bin % 2 );
 
