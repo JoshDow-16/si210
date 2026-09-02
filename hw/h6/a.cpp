04a2437c8e9feea57221848998731f1986b367d6
JoshDow-16 <m291920@usna.edu>
2026-09-02 12:45:44 -0400

    msg

---
+++ hw/h6/a.cpp
@@ -0,0 +1,15 @@
+// Joshua Downes m291920
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  double x = 10.5;
+  int n = 5;
+  char c = 'R';
+  string s = "Navy";
+  string t = "Army";
+  bool a = c == int('R');
+  cout << a << endl;
+  return 0;
+}
