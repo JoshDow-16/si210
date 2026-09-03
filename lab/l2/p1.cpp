3a55255b3bbc5c79d24c9189e6fa21ca846a4036
JoshDow-16 <m291920@usna.edu>
2026-09-03 08:02:44 -0400

    msg

--- lab/l2/p1.cpp
+++ lab/l2/p1.cpp
@@ -11,16 +11,16 @@ int main ()
   // Process 4-Bit Binary
   int res = ( bin / 1000 )*8 + ( bin % 1000 / 100 )*4 + ( bin % 100 / 10 )*2 + ( bin % 2 );
 
+  // Chesse with Hard-Coding :(
+  string binString;  
+  if ( res < 2 ) { binString = "000" + to_string( bin ); }
+  else if ( res < 4 ) { binString = "00" + to_string( bin ); }
+  else if ( res < 8 ) { binString = "0" + to_string( bin ); }
+  else { binString = to_string( bin ); }
+   
+
   // Output Result
 	cout << "In decimal " << binString << " = " << res << endl;
 
 	return 0;
 }
-
-/*either grab a string, and convert
-
-cant typecast, requires string parsing
-
-or grab an int and process
-
-have to check the length of the number using arithmetic*/
