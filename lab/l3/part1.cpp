22448e28d9dbae65860f7200ba40520cbfb643ae
JoshDow-16 <m291920@usna.edu>
2026-09-03 10:44:28 -0400

    msg

---
+++ lab/l3/part1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+
+  float x1, y1, x2, y2, x3, y3; char jnk;
+  cin >> jnk >> x1 >> jnk >> y1 >> jnk >> jnk >> x2 >> jnk >> y2 >> jnk >> jnk >> x3 >> jnk >> y3;
+  cout << x1 << ' ' << y1 << '\n' << x2 << ' ' << y2 << '\n' << x3 << ' ' << y3 << '\n' << x1 << ' ' << y1 << endl;
+
+  return 0;
+}
