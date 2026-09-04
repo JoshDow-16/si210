2b842a49d3e5925549576be0df7b7b14df33b40e
JoshDow-16 <m291920@usna.edu>
2026-09-03 20:34:36 -0400

    pluh

--- lab/l3/part1.cpp
+++ lab/l3/part1.cpp
@@ -3,10 +3,39 @@ using namespace std;
 
 int main ()
 {
-
+	// Grab Input, First Output
   float x1, y1, x2, y2, x3, y3; char jnk;
   cin >> jnk >> x1 >> jnk >> y1 >> jnk >> jnk >> x2 >> jnk >> y2 >> jnk >> jnk >> x3 >> jnk >> y3;
-  cout << x1 << ' ' << y1 << '\n' << x2 << ' ' << y2 << '\n' << x3 << ' ' << y3 << '\n' << x1 << ' ' << y1 << endl;
+  cout << x1 << ' ' << y1 << '\n' \
+			 << x2 << ' ' << y2 << '\n' \
+			 << x3 << ' ' << y3 << '\n' \
+			 << x1 << ' ' << y1 << "\n\n";
+
+	// Process Points
+  float high_x, low_x;
+	high_x = x1;
+	if ( x2 > high_x ) { high_x = x2; }
+	if ( x3 > high_x ) { high_x = x3; }
+
+	low_x = x1;
+	if ( x2 < low_x ) { low_x = x2; }
+	if ( x3 < low_x ) { low_x = x3; }
+
+	float high_y, low_y;
+	high_y = y1;
+	if ( y2 > high_y ) { high_y = y2; }
+	if ( y3 > high_y ) { high_y = y3; }
+
+	low_y = y1;
+	if ( y2 < low_y ) { low_y = y2; }
+	if ( y3 < low_y ) { low_y = y3; }
+
+	// Second Output
+	cout << low_x << ' ' << low_y << '\n' \
+			 << low_x << ' ' << high_y << '\n' \
+			 << high_x << ' ' << high_y << '\n' \
+			 << high_x << ' ' << low_y << '\n' \
+			 << low_x << ' ' << low_y << endl;
 
   return 0;
 }
--- lab/l3/part2.cpp
+++ lab/l3/part2.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main ()
 {
+	// Iteratively Grab Input and Process Data
   char type_a, op; int a;
   cin >> type_a >> a >> op;
 
@@ -12,28 +13,29 @@ int main ()
  		char type_b; int b;
     cin >> type_b >> b;
 
-		a = type_a == 'B' ? ( a / 1000 )*8 + ( a % 1000 / 100 )*4 + ( a % 100 / 10 )*2 + ( a % 2 ) : a
-		b = type_b == 'B' ? ( b / 1000 )*8 + ( b % 1000 / 100 )*4 + ( b % 100 / 10 )*2 + ( b % 2 ) : b
+		a = type_a == 'B' ? ( a / 1000 )*8 + ( a % 1000 / 100 )*4 + ( a % 100 / 10 )*2 + ( a % 2 ) : a;
+		b = type_b == 'B' ? ( b / 1000 )*8 + ( b % 1000 / 100 )*4 + ( b % 100 / 10 )*2 + ( b % 2 ) : b;
     res = a + b;
-
-
   }
 	else { res = a; }
 
-	char res_type;
+	// Assign Base Type and Format
+	char res_type; string zeroes = ""; 
 	if ( res < 16 )
 	{
-		int res_bin = 0; string zeroes = ""; bool numStart = false; res_type = 'B';
-	  if ( (num-8) >= 0 ) { res -= 8; res_bin += 1000; numStart = true; } else if ( !numStart ) { zeroes += '0'; }
-		if ( (num-4) >= 0 ) { res -= 4; res_bin += 100; numStart = true; } else if ( !numStart ) { zeroes += '0'; }
-	  if ( (num-2) >= 0 ) { res -= 2; res_bin += 10; numStart = true; } else if ( !numStart ) { zeroes += '0'; }
-		if ( (num-1) >= 0 ) { res_bin += 1; }
+	  int res_bin = 0; bool numStart = false; res_type = 'B';
+	  if ( ( res - 8 ) >= 0 ) { res -= 8; res_bin += 1000; numStart = true; } else if ( !numStart ) { zeroes += '0'; }
+		if ( ( res - 4 ) >= 0 ) { res -= 4; res_bin += 100; numStart = true; } else if ( !numStart ) { zeroes += '0'; }
+	  if ( ( res - 2 ) >= 0 ) { res -= 2; res_bin += 10; numStart = true; } else if ( !numStart ) { zeroes += '0'; }
+		if ( ( res - 1 ) >= 0 ) { res_bin += 1; }
+		res = res_bin;
 	}
 	else { res_type = 'D'; }
 
-
-
-	cout << res_type << res << endl;
+	// Output Result
+	cout << res_type;
+	if ( res_type == 'B' ) { cout << zeroes; }
+	cout << res << endl;
 
   return 0;
 }
