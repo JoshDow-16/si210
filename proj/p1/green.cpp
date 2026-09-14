e2fca0d568dc1f5ee8fc2e858e1c9d6704f4d5fe
JoshDow-16 <m291920@usna.edu>
2026-09-13 22:03:07 -0400

    msg

--- proj/p1/green.cpp
+++ proj/p1/green.cpp
@@ -17,6 +17,25 @@ int main ()
   string fhead, fhead2; file >> fhead; file2 >> fhead2;
   if ( !( fhead == "P3" && fhead2 == "P3" ) ) { cout << "Error: Input file wrong type" << endl; return 1; }
 
+	// Check File Sizes, Preprocessing
+  int w1, h1, w2, h2; file >> w1 >> h1; file2 >> w2 >> h2;
+  if ( w1 != w2 || h1 != h2 ) { cout << "Error: Input files off size" << endl; return 1; }
+  file >> fi; file2 >> fi2; out << "P3\n" << w1 << ' ' << h2 << '\n' << fi << endl;
+
+  // Processing
+	int r1, r2, g1, g2, b1, b2;
+  while ( file >> r1 >> g1 >> b1 )
+  {
+    file2 >> r2 >> g2 >> b2;
+    if ( !r1 && !b1 && g1 ) { out << r2 << ' ' << g2 << ' ' << b2; }
+    else { out << r1 << ' ' << g1 << ' ' << b1; }
+  }
+
+  // Housekeeping
+  file.close();
+  file2.close();
+  out << endl;		
+  out.close();
 
   return 0;
 }
