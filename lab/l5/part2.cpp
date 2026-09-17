f9f976efafb77f291130d21c264714f8584e31bc
JoshDow-16 <m291920@usna.edu>
2026-09-17 11:25:11 -0400

    msg

--- lab/l5/part2.cpp
+++ lab/l5/part2.cpp
@@ -6,14 +6,15 @@ using namespace std;
 int main ()
 {
   // Process. Output Result
-  int a = 1, b = 2, res, num;
+  int a = 0, b = 1, res, num;
   while (true )
   {
+    a = 0; b = 1;
     cout << "Enter total numbers in the Fibonacci series (0 to exit): "; cin >> num;
     if (!num ) { break; }
     if ( num < 0 ) { cout << "Not in domain!" << endl; continue; }
-    cout << "Result: 0 1 1 ";
-    for ( int i = 0; i < num; i++ ) {
+    cout << "Result: 0 1 ";
+    for ( int i = 2; i < num; i++ ) {
       res = a + b;
       cout << res << ' ';
       a = b; b = res;
